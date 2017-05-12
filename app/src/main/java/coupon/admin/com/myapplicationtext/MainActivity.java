de06155d905c9d7018f937247269be7710e7a49d
gdming123 <654144991@qq.com>
2017-05-12 15:58:57 +0800

    锦绣修改

--- app/src/main/java/coupon/admin/com/myapplicationtext/MainActivity.java
+++ app/src/main/java/coupon/admin/com/myapplicationtext/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //进行书
+        //进行书大大
     }
 }
